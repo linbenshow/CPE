e0362e5039ab8e55a3bb0303fdc5d8d6240acdd0
林聖邦 <66487660+linbenshow@users.noreply.github.com>
2025-03-07 00:51:19 +0800

    Update UVa00100.cpp

--- UVa00100.cpp
+++ UVa00100.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 using namespace std;
 
-int check(int n){
+int check(int n){//建立規則
     int len=1;
-    while (n!=1){
-        if(n%2==1){
+    while (n!=1){//條件為1則離開
+        if(n%2==1){//確認奇數
             n=3*n+1;
         }else {
             n/=2;
@@ -20,12 +20,12 @@ int main(int argc, char const *argv[]) {
     while(cin>>i>>j){
         Highlen=0;
         int m=i,k=j;
-        if(j<i){
+        if(j<i){//因為沒有規定i<j故要先確認正確
             swap(i,j);
         }
         for(int a=i,now;a<=j;a++){
             now=check(a);
-            if(now>Highlen){
+            if(now>Highlen){//更新最高數字
                 Highlen=now;
             }
         }
