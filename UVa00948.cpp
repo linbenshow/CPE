3a77c5486216115f479567f9da00bb6ff26f5a04
林聖邦 <66487660+linbenshow@users.noreply.github.com>
2025-03-15 17:10:39 +0800

    Create UVa00948.cpp

---
+++ UVa00948.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+    long long int fib[10000],N,ls[501],count;
+    fib[0]=1;
+    fib[1]=2;
+    for(int i=2;i<10000;i++)
+    {
+        fib[i]=fib[i-1]+fib[i-2];
+    }
+    cin>>N;
+    for(int i=0;i<N;i++)
+        cin>>ls[i];
+    for(int i=0;i<N;i++)
+    {
+        cout<<ls[i]<<" = ";
+        for(int j=0;ls[i]>=fib[j];j++)
+        {
+            count=j;
+        }
+        for (int j = count; j >= 0; j--)
+        {
+            if (ls[i]>=fib[j])
+            {
+                cout<<"1";
+                ls[i]=ls[i]-fib[j];
+            }
+            else
+            {
+                cout<<"0";
+            }  
+        }
+        cout<<" (fib)"<<endl;    
+    }
+
+    return 0;
+}
